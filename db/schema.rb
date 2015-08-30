ccb1ccef2b598377a8ed1c41e55c4c684152f3d4
bennacer860 <bennacer860@gmail.com>
2015-08-30 19:53:08 -0400

    generate user controller

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150830233859) do
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "name"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
